0749438a23caaa8722cd2f3200c998c05989ae50
anakafeel <hashmisaim037@gmail.com>
2024-05-05 18:54:25 -0400

    feat(WebsitePage): Render About section

--- stories/WebsitePage.tsx
+++ stories/WebsitePage.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 import { Main } from "./Main";
+import {About} from './About';
 
 export const WebsitePage = () => {
   return (
     <>
       <Header />
       <Main />
+      <About />
     </>
   );
 };
