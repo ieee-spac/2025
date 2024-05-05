91f13f2939f125898b4f15f9ac0848cbacd4407d
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-05 18:53:51 -0400

    feat(Main): Improve responsiveness

--- stories/Main.tsx
+++ stories/Main.tsx
@@ -4,17 +4,17 @@ import logo from "./assets/spac_logo_year_stars.svg";
 export const Main = () => {
   return (
     // Grid background container
-    <div className="min-h-screen flex justify-center items-center p-4 w-full dark:bg-black bg-white dark:bg-grid-green-600/[0.5] bg-grid-black/[0.2]">
+    <div className="min-h-screen flex justify-center items-center p-4 w-full dark:bg-black bg-white dark:bg-grid-green-600/[0.5] bg-grid-black/[0.2] overflow-none">
 
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)]"></div>
 
       {/* Center box */}
-      <main className="flex flex-col justify-center items-center space-y-4 h-fit border border-secondary px-4 py-8 md:px-20 md:py-16 rounded-lg transition-all ease-in-out duration-300 group hover:shadow-[0_0px_15px_rgba(0,202,255,1)] backdrop-blur-xs">
+      <main className="flex flex-col justify-center items-center space-y-4 h-fit border border-secondary px-4 py-8 md:px-20 md:py-16 w-full max-w-4xl rounded-lg transition-all ease-in-out duration-300 group hover:shadow-[0_0px_15px_rgba(0,202,255,1)] backdrop-blur-xs">
         <Image
           src={logo}
           alt="Main SPAC logo with year and stars"
-          className="w-full bobbing-animation-1 transition-all ease-in-out duration-700 group-hover:drop-shadow-[0_0px_5px_rgba(0,202,255,1)]"
+          className="w-full max-w-lg sm:max-w-xl bobbing-animation-1 transition-all ease-in-out duration-700 group-hover:drop-shadow-[0_0px_5px_rgba(0,202,255,1)]"
         />
 
         {/* Text */}
@@ -27,8 +27,6 @@ export const Main = () => {
             <a
               href="https://maps.app.goo.gl/wk5vdFH7StaCvsJf7"
               target="_blank"
-              noopener
-              noreferrer
               className="link"
             >
               Brookstreet Hotel
