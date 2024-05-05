10986ba62bc4513e4723aae5e8903ea682457fd8
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-05 10:42:19 -0400

    feat: Add backdrop blur to header

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -51,24 +51,29 @@ export const Header = () => {
   };
 
   return (
-    <header className="navbar fixed border-b-[0.25px] border-b-secondary overflow-hidden px-3 md:px-8">
-      <div className="navbar-start">
-        <Image src={Logo} alt="IEEE SPAC Logo" className="w-16 md:w-20" />
-      </div>
+    <>
+      {/* Top Header */}
+      <header className="navbar fixed border-b-[0.25px] border-b-secondary backdrop-blur-sm overflow-hidden px-3 md:px-8">
+        <div className="navbar-start">
+          <Image src={Logo} alt="IEEE SPAC Logo" className="w-16 md:w-20" />
+        </div>
 
-      <div className="navbar-end">
-        <HamburgerButton menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <div className="navbar-end">
+          <HamburgerButton menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        </div>
+      </header>
 
-        {/* Navigation Menu for both Desktop and Mobile*/}
-        <nav>
-          {/* The menu tag is the same as ul, just a bit for semantic for the context of navbars */}
-          <menu
-            className={`menu menu-vertical md:menu-horizontal space-y-4 md:space-y-0 md:space-x-4 border border-secondary border-opacity md:border-none p-4 fixed top-20 md:top-0 right-0 w-fit backdrop-blur-md md:backdrop-blur-none rounded-xl transition transform ease-in-out duration-700 ${menuOpen ? "mr-4" : "translate-x-full md:translate-x-0"}`}
-          >
-            <NavigationLinks onCloseMenu={() => setMenuOpen(false)} />
-          </menu>
-        </nav>
-      </div>
-    </header>
+      {/* Navigation Menu for both Desktop and Mobile*/}
+      <nav>
+        {/* The menu tag is the same as ul, just a bit for semantic for the context of navbars */}
+        <menu
+          className={`menu menu-vertical md:menu-horizontal space-y-4 md:space-y-0 md:space-x-4 border border-secondary border-opacity md:border-none p-4 fixed top-20 md:top-0 right-0 w-fit backdrop-blur-md md:backdrop-blur-none rounded-xl transition transform ease-in-out duration-700 z-50 ${menuOpen ? "mr-4" : "translate-x-full md:translate-x-0"}`}
+        >
+          <NavigationLinks onCloseMenu={() => setMenuOpen(false)} />
+        </menu>
+      </nav>
+
+      {/* The reason why the menu navigation menu is not nested inside the header tag is because the stacking contexts created by the backdrop blur property causes the menu tag to not render, hence why they cannot be nested. */}
+    </>
   );
 };
