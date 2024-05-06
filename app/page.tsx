b914850f024594f9af2507236bb987d98d92c287
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-06 02:28:03 -0400

    feat: Render components in Next.js App

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-'use client'
+import {Header} from '../stories/Header'
+import {Main} from '../stories/Main'
+import {About} from '../stories/About'
+import {PastPatrons} from '../stories/PastPatrons'
+import {Contact} from '../stories/Contact'
+import {Footer} from '../stories/Footer'
 
 export default function Home() {
   return (
-    <main className="flex flex-wrap min-h-screen items-center justify-between p-24">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent btn-outline">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </main>
+   <>
+      <Header />
+      <Main />
+      <About />
+      <PastPatrons />
+      <Contact />
+      <Footer />
+    </> 
   );
 }
