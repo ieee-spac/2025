5b1dbfe2b08f3538207f87a9078502578c10b79a
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-06 00:12:13 -0400

    feat(Contact): Add shadow to browser container and focus colors for input boxes

--- stories/Contact.tsx
+++ stories/Contact.tsx
@@ -4,56 +4,57 @@ export const Contact = () => {
       <h3 className="text-primary text-5xl sm:text-6xl font-bold">Contact</h3>
 
       {/* Browser Container*/}
-      <div className="mockup-browser border border-base-300">
+      <div className="mockup-browser border border-base-300 transition-all duration-700 hover:shadow-[0_0px_15px_rgba(0,202,255,0.5)]">
         {/* Browser Toolbar */}
         <div className="mockup-browser-toolbar">
-          <p className="input border border-base-300">
-          Get in touch :)
-          </p>
+          <p className="input border border-base-300">Get in touch :)</p>
         </div>
 
         {/* Form Container */}
-          <form action="" className="space-y-5 w-full flex flex-col justify-center items-center px-4 py-16 border-t border-base-300">
-            {/* Full Name */}
-            <label className="form-control w-full max-w-lg">
-              <div className="label">
-                <span className="label-text text-base">Full Name</span>
-              </div>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered"
-              />
-            </label>
+        <form
+          action=""
+          className="space-y-5 w-full flex flex-col justify-center items-center px-4 py-16 border-t border-base-300"
+        >
+          {/* Full Name */}
+          <label className="form-control w-full max-w-lg">
+            <div className="label">
+              <span className="label-text text-base">Full Name</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered focus:input-secondary"
+            />
+          </label>
 
-            {/* E-Mail Address */}
-            <label className="form-control w-full max-w-lg">
-              <div className="label">
-                <span className="label-text text-base">E-Mail Address</span>
-              </div>
-              <input
-                type="E-Mail Address"
-                placeholder="Type here"
-                className="input input-bordered"
-              />
-            </label>
+          {/* E-Mail Address */}
+          <label className="form-control w-full max-w-lg">
+            <div className="label">
+              <span className="label-text text-base">E-Mail Address</span>
+            </div>
+            <input
+              type="E-Mail Address"
+              placeholder="Type here"
+              className="input input-bordered focus:input-secondary"
+            />
+          </label>
 
-            {/* Message */}
-            <label className="form-control w-full max-w-lg">
-              <div className="label">
-                <span className="label-text text-base">Message</span>
-              </div>
-              <textarea
-                className="textarea textarea-bordered"
-                placeholder="Type here"
-              ></textarea>
-            </label>
+          {/* Message */}
+          <label className="form-control w-full max-w-lg">
+            <div className="label">
+              <span className="label-text text-base">Message</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered focus:input-secondary text-base"
+              placeholder="Type here"
+            ></textarea>
+          </label>
 
-            {/* Submit Button */}
-            <button className="btn btn-outline btn-accent btn-wide text-base">
-              Submit
-            </button>
-          </form>
+          {/* Submit Button */}
+          <button className="btn btn-outline btn-accent btn-wide text-base md:text-lg">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
