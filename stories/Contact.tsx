e27349f38f682338ac5d86892554655ff4a0f65a
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-05 22:15:51 -0400

    feat(Contact): Create and style contact form

---
+++ stories/Contact.tsx
@@ -0,0 +1,60 @@
+export const Contact = () => {
+  return (
+    <div className="px-3 md:px-8 max-w-3xl mx-auto space-y-10 overflow-none my-20">
+      <h3 className="text-primary text-5xl sm:text-6xl font-bold">Contact</h3>
+
+      {/* Browser Container*/}
+      <div className="mockup-browser border border-base-300">
+        {/* Browser Toolbar */}
+        <div className="mockup-browser-toolbar">
+          <p className="input border border-base-300">
+          Get in touch :)
+          </p>
+        </div>
+
+        {/* Form Container */}
+          <form action="" className="space-y-5 w-full flex flex-col justify-center items-center px-4 py-16 border-t border-base-300">
+            {/* Full Name */}
+            <label className="form-control w-full max-w-lg">
+              <div className="label">
+                <span className="label-text text-base">Full Name</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered"
+              />
+            </label>
+
+            {/* E-Mail Address */}
+            <label className="form-control w-full max-w-lg">
+              <div className="label">
+                <span className="label-text text-base">E-Mail Address</span>
+              </div>
+              <input
+                type="E-Mail Address"
+                placeholder="Type here"
+                className="input input-bordered"
+              />
+            </label>
+
+            {/* Message */}
+            <label className="form-control w-full max-w-lg">
+              <div className="label">
+                <span className="label-text text-base">Message</span>
+              </div>
+              <textarea
+                className="textarea textarea-bordered"
+                placeholder="Type here"
+              ></textarea>
+            </label>
+
+            {/* Submit Button */}
+            <button className="btn btn-outline btn-accent btn-wide text-base">
+              Submit
+            </button>
+          </form>
+      </div>
+    </div>
+  );
+};
