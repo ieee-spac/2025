1661b452f87e351797704ac06a36cf49beda057e
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-05 20:40:56 -0400

    feat(PastPatrons): Create and style section

---
+++ stories/PastPatrons.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { PastPatrons } from './PastPatrons';
+
+const meta = {
+  title: 'Website/PastPatrons',
+  component: PastPatrons,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  args: {
+  },
+} satisfies Meta<typeof PastPatrons>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+
+};
