2c7836e216d1707604325c5a87ffc53562ae0055
Mumtahin Farabi <mfarabi619@gmail.com>
2024-05-25 18:38:43 -0400

    refactor(app): Convert relative paths to path aliases

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import {Header} from '../stories/Header'
-import {Main} from '../stories/Main'
-import {About} from '../stories/About'
-import {PastPatrons} from '../stories/PastPatrons'
-import {Contact} from '../stories/Contact'
-import {Footer} from '../stories/Footer'
+import {Header} from '@/stories/Header'
+import {Main} from '@/stories/Main'
+import {About} from '@/stories/About'
+import {PastPatrons} from '@/stories/PastPatrons'
+import {Contact} from '@/stories/Contact'
+import {Footer} from '@/stories/Footer'
 
 export default function Home() {
   return (
