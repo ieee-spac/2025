a5205b96251012bbcc7ddfb45399663dbd16a80a
anakafeel <hashmisaim037@gmail.com>
2024-06-09 09:22:21 -0400

    fix(Hero): Green grid spilling on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import './globals.css';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Open_Sans } from 'next/font/google';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'SPAC 2024',
+
   description:
     'Official Website of the IEEE Student Professional Awareness Conference 2024, hosted in Ottawa, Ontario.',
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: 'device-width',
+  maximumScale: 1,
 };
 
 export default function RootLayout({
@@ -18,7 +25,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>{children}</body>
+      {' '}
+      <body className={openSans.className}>{children}</body>{' '}
     </html>
   );
 }
--- src/components/sections/home/hero/hero.tsx
+++ src/components/sections/home/hero/hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
     // <div className="min-h-screen flex justify-center items-center p-4 w-full dark:bg-black bg-white dark:bg-grid-green-600/[0.5] bg-grid-black/[0.2] overflow-none">
     <div className="overflow-none flex h-screen w-full items-center justify-center bg-black p-4 bg-grid-green-600/[0.5]">
       {/* Radial gradient for the container to give a faded look */}
-      {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)]"></div> */}
+      {/* <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] dark:bg-black" /> */}
       <div className="pointer-events-none absolute inset-0 flex h-screen items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)]" />
 
       {/* Center box */}
