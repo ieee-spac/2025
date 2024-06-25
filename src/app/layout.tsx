3d59aa93fc4401cca1b9247b992b6e020d9690b1
anakafeel <hashmisaim037@gmail.com>
2024-06-24 22:16:01 -0400

    chore(Repo): Add Vercel Speed Insights & upversion packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata, Viewport } from 'next';
 import { Open_Sans } from 'next/font/google';
 
@@ -25,8 +26,11 @@ export default function RootLayout({
 }) {
   return (
     <html className="scroll-pt-20 scroll-smooth md:scroll-pt-28" lang="en">
-      <body className={openSans.className}>{children}</body>
-      <Analytics />
+      <body className={openSans.className}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
