c035c398b8af8f37b3244a1e18669f4aef3c3deb
Mumtahin Farabi <mfarabi619@gmail.com>
2024-09-08 12:02:44 -0400

    feat: improve email template formatting

--- src/components/templates/email.tsx
+++ src/components/templates/email.tsx
@@ -20,17 +20,21 @@ export const EmailTemplate: React.FC<
               {/* Message to sender from SPAC 2024 Team */}
               <p>
                 Hello
+                {' '}
                 {fullName}
                 ,
               </p>
               <p>
                 Thank you for taking the time to fill out the form on our website.
                 We&apos;ve received your message and will get back to you as soon as
-                possible if a response is required! If you have any questions,
+                possible if a response is required!
+              </p>
+              <p>
+                If you have any questions,
                 concerns, or clarifications, replying to this email will send your
                 message to the SPAC 2024 team.
               </p>
-              <p>Here is a copy of your message.</p>
+              <p>Here is a copy of your message:</p>
               <p>
                 <strong>Subject: </strong>
                 {subject}
