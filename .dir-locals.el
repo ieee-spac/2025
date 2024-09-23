5654ab2ee90948056fe7581904f1d5e795cd10aa
anakafeel <hashmisaim037@gmail.com>
2024-09-23 12:26:10 -0400

    fix(ui/components/patrons): change blackberry logo to white wordmark no background

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; Directory local variables
+;; For more information see (info "(emacs) Directory Variables")
+
+((auto-mode-alist . (("\\.mdx\\'" . markdown-mode)))
+ (markdown-mode . ((fill-column . 80)
+                   (eval . (prettier-js-mode 1))))
+ (typescript-mode . ((typescript-indent-level . 2))))
