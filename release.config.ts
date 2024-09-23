72f575ccd1b1a8cceff1be3a56b028c0f2f5ecbc
Mumtahin Farabi <mfarabi619@gmail.com>
2024-09-23 12:43:58 -0400

    ci: split up jobs into separate pipelines

--- release.config.ts
+++ release.config.ts
@@ -2,6 +2,7 @@ import type { GlobalConfig } from 'semantic-release'
 
 const config: Partial<GlobalConfig> = {
   branches: ['main'],
+  repositoryUrl: 'https://github.com/ieee-spac/2024',
 }
 
 export default config
