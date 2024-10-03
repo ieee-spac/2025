b8e39cf37b8c18558b95204fef5a7b361c18398c
anakafeel <hashmisaim037@gmail.com>
2024-10-03 12:41:03 -0400

    feat(components/aceternity/input): add classes for theme switches

--- src/components/aceternity/input.tsx
+++ src/components/aceternity/input.tsx
@@ -40,12 +40,12 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            `flex h-10 w-full border bg-background text-black dark:text-white shadow-input rounded-sm px-3 py-2 text-sm file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-info
-           disabled:cursor-not-allowed disabled:opacity-50
-           dark:shadow-[0px_0px_1px_1px_var(--sky-950)]
-           group-hover/input:shadow-none transition duration-400
+            `flex h-10 w-full px-3 py-2 text-sm text-black dark:text-white
+             border bg-background shadow-primary dark:shadow-[0px_0px_1px_1px_var(--sky-950)] rounded-sm
+             file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600
+             focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-primary dark:focus-visible:ring-info
+             disabled:cursor-not-allowed disabled:opacity-50
+             group-hover/input:shadow-none transition duration-400
            `,
             className,
           )}
