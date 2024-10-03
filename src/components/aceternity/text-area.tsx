3c3e3b97583e6ce1249b7d08308967749f12d71e
Mumtahin Farabi <mfarabi619@gmail.com>
2024-10-03 12:41:15 -0400

    feat(components/aceternity/text-area): add classes for theme switches

--- src/components/aceternity/text-area.tsx
+++ src/components/aceternity/text-area.tsx
@@ -48,9 +48,9 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       >
         <textarea
           className={cn(
-            `flex min-h-[80px] w-full border dark:bg-background text-black dark:text-white shadow-input rounded-sm px-3 py-2 text-sm  file:border-0 file:bg-transparent
+            `flex min-h-[80px] w-full border bg-background text-black dark:text-white shadow-primary rounded-sm px-3 py-2 text-sm  file:border-0 file:bg-transparent
           file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-info
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-primary dark:focus-visible:ring-primary
           disabled:cursor-not-allowed disabled:opacity-50
           dark:shadow-[0px_0px_1px_1px_var(--sky-950)]
           group-hover/input:shadow-none transition duration-400
