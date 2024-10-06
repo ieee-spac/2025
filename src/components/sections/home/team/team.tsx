7852476dd4ea683b4e96b383574ec7bafd59dd30
Mumtahin Farabi <mfarabi619@gmail.com>
2024-10-06 13:05:26 -0400

    feat(ui/team): change team section title to 'Meet the Team'

--- src/components/sections/home/team/team.tsx
+++ src/components/sections/home/team/team.tsx
@@ -8,7 +8,7 @@ export function Team() {
   return (
     <div id="team" className="mx-auto mt-20 max-w-3xl px-3 md:px-8">
       <h2 className="mb-10 text-5xl font-bold text-primary sm:text-6xl">
-        Our Team
+        Meet the Team
       </h2>
 
       <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3">
