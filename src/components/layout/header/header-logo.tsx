fd70de47e722dd2383bc09f5594643704750b3ec
anakafeel <hashmisaim037@gmail.com>
2025-08-05 20:59:10 +0300

    feat(ui/patrons): add previous patrons page

--- src/components/layout/header/header-logo.tsx
+++ src/components/layout/header/header-logo.tsx
@@ -6,7 +6,7 @@ export function HeaderLogo({ logo }: { logo: string }) {
     <Link href="/">
       <Image
         src={logo}
-        alt="SPAC 2024 Logo"
+        alt="SPAC 2025 Logo"
         className="h-10 w-auto transition-all duration-500 ease-in-out hover:scale-110 md:h-16"
         height={40}
         width={40}
